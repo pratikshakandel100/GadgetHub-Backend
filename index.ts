7691ade07d90b1f350c34305dfa4198ced60e55c
Pratiksha Kandel <pratikshakandel100@gmail.com>
2026-06-09 14:52:13 +0545

    done some changes

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import app from './src/app';
+import dotenv from "dotenv";
 
 import { PORT as SERVER_PORT } from './src/config/constant';
 import { connectToMongoDB } from './src/database/mongodb';
 
+dotenv.config();
 connectToMongoDB()
     .then(()=>{
         console.log("MongoDB connection established, starting server....")
@@ -11,9 +13,9 @@ connectToMongoDB()
         console.log("Failed to connect to MongoDB, server not started", error);
     });
 
-    app.listen(
-        SERVER_PORT,
-        ()=>{
-            console.log(`Server running: ${SERVER_PORT}`);
-        }
-    )
+   app.listen(
+    SERVER_PORT,  // start backend in this PORT
+    () => {
+        console.log(`Server: http://localhost:${SERVER_PORT}`); // backtick
+    }
+);
