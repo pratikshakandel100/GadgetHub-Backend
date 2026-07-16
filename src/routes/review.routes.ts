60f3b5716eb2969c1460bd22582f399f0e1f1767
Pratiksha Kandel <pratikshakandel100@gmail.com>
2026-07-16 11:48:51 +0545

    add multer image upload middleware to the create routr and a public product-reviews route

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -2,12 +2,19 @@ import { Router } from "express";
 import { ReviewController } from "../controller/review.controller";
 import { authorizedMiddleware } from "../middlewares/authorized.middleware";
 import { adminMiddleware } from "../middlewares/admin.middleware";
+import { uploads } from "../middlewares/upload.middleware";
 
 const reviewRouter = Router();
 const reviewController = new ReviewController();
 
-reviewRouter.post("/", authorizedMiddleware, reviewController.createReview);
+reviewRouter.post(
+    "/",
+    authorizedMiddleware,
+    uploads.fields([{ name: "images", maxCount: 5 }]),
+    reviewController.createReview
+);
 reviewRouter.get("/my", authorizedMiddleware, reviewController.getMyReviews);
+reviewRouter.get("/product/:productId", reviewController.getProductReviews);
 reviewRouter.get("/admin/all", authorizedMiddleware, adminMiddleware, reviewController.getAllReviews);
 reviewRouter.patch("/:id/status", authorizedMiddleware, adminMiddleware, reviewController.updateReviewStatus);
 reviewRouter.delete("/:id", authorizedMiddleware, adminMiddleware, reviewController.deleteReview);
